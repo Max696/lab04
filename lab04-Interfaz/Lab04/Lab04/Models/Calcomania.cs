8e1ac70659eba15ea4dca079c8704adaccb35626
CarlosLam <beca2019a@gmail.com>
2018-04-15 20:47:52 -0700

    ...

---
+++ lab04-Interfaz/Lab04/Lab04/Models/Calcomania.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lab04.Models
+{
+    public class Calcomania
+    {
+        
+        public string nombre { get; set; }
+       [Display(Name= "Encontrado")]
+        public bool existe { get; set; }
+         [Display(Name = "Numero")]
+        public int id { get; set; }
+       
+    }
+}
